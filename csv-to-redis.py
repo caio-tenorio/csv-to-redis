dfe4fcad32bb2ed70d5b259dc46eb802d43f704d
Caio <cavalcanti971@gmail.com>
2018-06-19 00:28:36 -0300

    mudanças nas formações das chaves e adição de script runall

--- csv-to-redis.py
+++ csv-to-redis.py
@@ -1,27 +1,44 @@
 import csv, redis, json
 import sys
 
-REDIS_HOST = 'localhost'
+REDIS_HOST = '172.17.0.2'
+ORGAOS = "orgaos-II"
+SUGESTAO = "sugestao-I"
+TURMAS_COMPONETES = "turmas-componentes-III"
 
 def read_csv_data(csv_file):
     with open(csv_file, encoding='utf-8') as csvf:
         csv_data = csv.reader(csvf)
         return [(r) for r in csv_data]
 
-def store_data(conn, data, key_index):
+def store_data(conn, data, key_index, db_name):
     if type(key_index) != list:
-        for i in data:
-            conn.setnx(i[int(key_index)], i)    
+        for line in data:
+            key = db_name + "_" + line[int(key_index)]
+            conn.setnx(key, line)    
     else:
         for line in data:
-            key = ""
+            key = db_name + "_"
             for c in key_index:
                 key = key + line[int(c)]
             conn.setnx(key, line)
     return data        
 
+def which_db(csv_file):
+    db = ""
+    if csv_file == ORGAOS:
+        db = "orgaos"
+    elif csv_file == SUGESTAO:
+        db = "sugestao"
+    elif csv_file == TURMAS_COMPONETES:
+        db = "turmas-componentes"
+    return db
+
 def main():
     args_len = len(sys.argv)
+    csv_file = sys.argv[1].split("/")[-1][:-4]
+    db_name = which_db(csv_file)
+
     if args_len < 2:
         sys.exit(
             "Usage: %s file.csv key key key..."
@@ -34,9 +51,15 @@ def main():
             if i != 0 and i != 1:
                 key_index.append(sys.argv[i])
     
-    data = read_csv_data(sys.argv[1])
-    conn = redis.Redis(REDIS_HOST)
-    print (json.dumps(store_data(conn, data, key_index)))
+    try:
+        data = read_csv_data(sys.argv[1])
+        conn = redis.Redis(REDIS_HOST)
+        entry_data = (json.dumps(store_data(conn, data, key_index, db_name)))
+        print ("Dados registrados com sucesso!")
+        pass
+    except Exception as e:
+        raise print("Ocorreu um problema...")
+    
 
 if '__main__' == __name__:
     main()
---
+++ run-all.sh
@@ -0,0 +1,3 @@
+python3 csv-to-redis.py ~/Documentos/microservices/sugestao-I.csv 0 1
+python3 csv-to-redis.py ~/Documentos/microservices/orgaos-II.csv 0 1
+python3 csv-to-redis.py ~/Documentos/microservices/turmas-componentes-III.csv 0 1 3
